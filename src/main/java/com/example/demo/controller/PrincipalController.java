4c8c96937d6911222b85239bddf3b518d3d64b00
Alfred <anthonysub300@gmail.com>
2023-07-23 23:35:11 -0500

    Avanc1_pagina principal

---
+++ src/main/java/com/example/demo/controller/PrincipalController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/inicios")
+public class PrincipalController {
+	
+	@GetMapping("/principal")
+	public String principal() {// falta el argumento
+		return "vistaPrincipal";
+	}
+	
+	@GetMapping("/ingreso")
+	public String ingreso() {// falta el argumento
+		return "vistaIngreso";
+	}
+	
+	@GetMapping("/suscripcion")
+	public String suscripcion() {// falta el argumento
+		return "vistaSuscripcion";
+	}
+	
+	@GetMapping("/donacion")
+	public String donacion() {// falta el argumento
+		return "vistaDonacion";
+	}
+	
+	@GetMapping("/informacion")
+	public String informacion() {// falta el argumento
+		return "vistaInformacion";
+	}
+	
+	@GetMapping("/reservas")
+	public String reservas() {// falta el argumento
+		return "vistaReservas";
+	}
+
+}
